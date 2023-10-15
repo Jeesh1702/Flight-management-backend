0212c466a65181ea69c9d7cc5b7035904821883e
Jeesh1702 <spjeeshnu@gmail.com>
2023-10-15 13:41:07 +0530

    project init

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("hello world")
+import app from "./server.js"
+import mongodb from "mongodb"
+import dotenv from "dotenv"
+
+dotenv.config()
+const MongoClient = mongodb.MongoClient
+
+const port = process.env.PORT || 8000
+
+MongoClient.connect(
+    process.env.RESTREVIEWS_DB_URI,
+    {
+        maxPoolSize: 50,
+        wtimeoutMS: 2500,
+        useNewUrlParser: true
+    }
+)
+.catch(err => {
+   console.error(err.stack)
+   process.exit(1) 
+})
+.then(async client => {
+    // await providerDAO.injectDB(client)
+    app.listen(port, () => {
+        console.log(`server listening on port ${port}`)
+    })
+})
